7247d77e629b526734987ba2d077996589306c82
Tamal Saha <tamal@appscode.com>
2020-08-17 00:20:59 -0700

    Add product variables
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- info/lib.go
+++ info/lib.go
@@ -14,8 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package issuer
+package info
 
 var (
 	LicenseCA string
+
+	ProductOwnerName string
+	ProductOwnerUID  string
+
+	ProductName string
+	ProductUID  string
 )
